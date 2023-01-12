500667769f540fefd2fc28181a9205a6cb64d357
Demirkol, Goerkem <gdemirkol315@gmail.com>
2023-01-12 21:37:15 +0100

    elevator motion simulated

--- src/main/java/Model/Elevator.java
+++ src/main/java/Model/Elevator.java
@@ -3,14 +3,13 @@ package Model;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class Elevator {
 
     private String id;
 
-    private int currentFloor;
+    private int currentFloor = 0;
+    private State state = State.IDLE;
     private Direction currentDirection;
     private final Logger logger = LogManager.getLogger(Elevator.class);
 
@@ -19,13 +18,37 @@ public class Elevator {
 
     public Elevator(String id) {
         this.id = id;
-        currentFloor = 0;
     }
 
     public void move(int destinationFloor) {
 
-        logger.info("Elevator no: {} is moving from level {} to {}",id, currentFloor, destinationFloor);
-        currentFloor = destinationFloor;
+        logger.info("Elevator no: {} is moving from level {} to {}", id, currentFloor, destinationFloor);
+
+        if (currentFloor - destinationFloor < 0) {
+            currentDirection = Direction.UP;
+        } else {
+            currentDirection = Direction.DOWN;
+        }
+
+        try {
+            while (currentFloor != destinationFloor) {
+                state = State.IN_MOTION;
+
+                if (currentDirection.equals(Direction.UP)) {
+                    currentFloor++;
+                    Thread.sleep(500);
+                } else if (currentDirection.equals(Direction.DOWN)) {
+                    currentFloor--;
+                    Thread.sleep(500);
+                }
+
+                logger.debug("Elevator no: {} in motion floor {}", id, currentFloor);
+
+            }
+            state = State.IDLE;
+        } catch (InterruptedException e) {
+            logger.error("Exception occurred elevator cannot move!");
+        }
 
     }
 
---
+++ src/main/java/Model/State.java
@@ -0,0 +1,6 @@
+package Model;
+
+public enum State {
+    IN_MOTION,
+    IDLE
+}
